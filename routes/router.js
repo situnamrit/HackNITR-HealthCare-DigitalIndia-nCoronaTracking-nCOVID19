aeb0c0eba0b7eb25c6f37275ce2a4a48e1ebfbee
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-03-21 18:07:02 +0530

    :zap: build: custom api for fetch requests

---
+++ routes/router.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const moment = require("moment");
+const router = new express.Router();
+const request = require("request");
+
+const place = require("../src/utils/place");
+const tracker = require("../src/utils/tracker");
+
+const image2 =
+    "https://images.unsplash.com/photo-1580483733209-558da93c143c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+const image1 =
+    "https://images.unsplash.com/photo-1583072248822-f909497b6ab6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+
+router.get("/", (req, res) => {
+    res.render("index");
+});
+
+router.get("/covid", (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: "You gotta enter an address to search 🤷‍♂️"
+        });
+    }
+
+    tracker(req.query.search, (error, data) => {
+        if (error) {
+            return res.send({
+                image1: image1,
+                image2: image2,
+                totalConfirmed: data.confirmed,
+                totalDeaths: data.deaths,
+                totalRecovered: data.recovered
+            });
+        }
+
+        let date = moment(data.last_updated);
+
+        place(req.query.search, (error, img) => {
+            if (error) {
+                return res.send({
+                    image1: image1,
+                    image2: image2,
+                    place: data.province,
+                    country: data.country,
+                    updated: date.fromNow(),
+                    confirmed: data.latest.confirmed,
+                    deaths: data.latest.deaths,
+                    recovered: data.latest.recovered
+                });
+            }
+
+            res.send({
+                image1: img[0],
+                image2: img[1],
+                place: data.province,
+                country: data.country,
+                updated: date.fromNow(),
+                confirmed: data.latest.confirmed,
+                deaths: data.latest.deaths,
+                recovered: data.latest.recovered
+            });
+        });
+    });
+});
+
+module.exports = router;
