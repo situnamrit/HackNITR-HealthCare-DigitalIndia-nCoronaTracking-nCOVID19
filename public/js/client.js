34b9d8581e94b93dae1a7175b9a0d47a98fc0dd0
Amrit Sahani <situnamrit@gmail.com>
2020-03-22 03:46:35 +0530

    .

--- public/js/client.js
+++ public/js/client.js
@@ -49,7 +49,7 @@ panicForm.addEventListener("submit", e => {
     if (days <= 4) {
         if (cough || fever) {
             box.textContent =
-                "These are very mild symptomps which may even be just cold or normal Flu. So have patience and faith, there's nothing to worry about. ✌";
+                "These are very mild symptomps which may even be just cold or normal Flu. So have patience and faith, there's nothing to worry about.";
         } else if (tired) {
             box.textContent =
                 "It's been hell of a pandemic, maybe you just need to rest a little bit more. We're here for you ❤";
@@ -60,10 +60,30 @@ panicForm.addEventListener("submit", e => {
             box.textContent =
                 "Sorry, we gave our best, but still its hard saying your situation. Prefer to contact our specialists below. 😕";
         }
-    } else if (days ==4 && cough) {
+    } else if (days ==4 && fever) {
         box.textContent =
             "You may not be properly taking rest and drink a lots of hot water Avoid cold items ";
-    } else {
+    }
+    else if (days ==4 && cough) {
+        box.textContent =
+            "You should be consulting a medication specialists or chest specialists to be safe enough";
+    }
+
+    else if (days ==4 && breathe) {
+        box.textContent =
+            "Please see to a chest specialists as breathe is not good";
+    }
+
+     else if (days ==4 && breathe && fever) {
+        box.textContent =
+            "These are very mild symptomps which may even be just cold or normal Flu. So have patience and faith, there's nothing to worry about.;
+    }
+     else if (days == 3 && breathe)
+     {
+     	box.textContent = "These are very mild symptomps which may even be just cold or normal Flu. So have patience and faith, there's nothing to worry about.";
+     }
+
+     else {
         box.textContent =
             "Um...somethings not right with us. Maybe you could give us call as soon as possible. 📞";
     }
